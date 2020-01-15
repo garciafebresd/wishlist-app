4b3fa351cb9b12ce5513b01657158cc6adc07db7
daniel garcia <dgarcia@position.cl>
2020-01-15 20:05:26 -0300

    add Item page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+  },  {
+    path: 'add-item',
+    loadChildren: () => import('./pages/add-item/add-item.module').then( m => m.AddItemPageModule)
   }
+
 ];
 @NgModule({
   imports: [
