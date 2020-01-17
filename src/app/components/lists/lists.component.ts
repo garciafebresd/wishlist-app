47754e23dcc0870478f02220636960429037ac53
daniel garcia <dgarcia@position.cl>
2020-01-17 18:56:07 -0300

    app done

--- src/app/components/lists/lists.component.ts
+++ src/app/components/lists/lists.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { List } from '../../models/list.model';
 import { WishesService } from '../../services/wishes.service';
 import { Router } from '@angular/router';
+import { AlertController, IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-lists',
@@ -10,10 +11,12 @@ import { Router } from '@angular/router';
 })
 export class ListsComponent implements OnInit {
 
+  @ViewChild(IonList, {static: false}) ionLista: IonList;
   @Input() completed: true;
 
   constructor(public wishesService: WishesService,
-              public router: Router) { }
+              public router: Router,
+              private alertController: AlertController) { }
 
   ngOnInit() { }
 
@@ -30,8 +33,46 @@ export class ListsComponent implements OnInit {
     }
   }
 
-  removeItem(list: List){
+  removeItem(list: List) {
     this.wishesService.removeList(list);
+  }
+
+  async editList(list: List) {
+    const alert = await this.alertController.create({
+      header: 'Editar lista',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          placeholder: 'Nombre de la lista',
+          value: list.title
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('cancelar');
+          }
+        },
+        {
+          text: 'Editar',
+          handler: (data) => {
+            console.log(data);
+            if (data.titulo.length === 0) {
+              return;
+            }
+
+            list.title = data.titulo;
+            this.wishesService.saveStorage();
+            this.ionLista.closeSlidingItems();
+          }
+        }
+      ]
+    });
+
+    alert.present();
   }
 
 }
